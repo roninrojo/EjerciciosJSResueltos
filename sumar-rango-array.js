8da1542a386704104bdbbe07c71ac241c5ccccf7
acarazo <alberto.carazo@gmail.com>
2023-03-29 13:34:19 +0200

    Sumar rango. Comentarios

--- sumar-rango-array.js
+++ sumar-rango-array.js
@@ -1,15 +1,19 @@
+/*
+Sumar el rango de números entre dos números pasados por un array. No tienen porqué estar ordenados.
+Ej: [2,8] -> 2+3+4+5+6+7+8 = 35
+*/
 function sumAll(arr) {
+	// Primero ordenamos el array
 	const orderArr = arr.sort((a, b) =>  a - b );
-	console.log(orderArr);
+	// Extrameos el valor inicial y final
 	const [a, b] = orderArr;
+	// Insertamos en un array los valores intermedios entre el inicio y el final
 	for (i=a+1; i < b; i++) {
 		arr.push(i)
 	}
-	const newArr = arr.sort((a, b) => a - b ).reduce((acc,num)=> acc+num);
-	console.log(newArr);
-	
-  return 1;
+	// Devolcemos la suma de los valores del array mediante la función reduce.
+  	return arr.reduce((acc,num)=> acc+num);;
 }
 
-sumAll([4, 1]);
+console.log(sumAll([8, 2]));
 
