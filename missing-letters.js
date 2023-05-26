57f949b48debdf2ffcc6eb803bb4a3f23613df36
acarazo <alberto.carazo@gmail.com>
2023-05-26 14:19:37 +0200

    missing-letters

--- missing-letters.js
+++ missing-letters.js
@@ -5,13 +5,19 @@ Si todas las letras están presentes en la cadena, devuelve undefined.
 
 function fearNotLetter(str) {
     const abc = "abcdefghijklmnopqrstuvwxyz"
+
+    // Identifico donde comienza la cadena, usando el primer carácter de str
     const index = abc.indexOf(str.charAt(0))
+
+    // luego extraigo la cadena equivalente de abc
     const sliced = abc.slice(index, index + str.length)
     
+    // filtro la cadena extraída identificando el caracte que no extá incluído en las dos
     const missed = sliced.split('').filter(item => {
         return !str.split('').includes(item)
     }).toString();
 
+    // Si no se devuelve nada, es undefined
     return (missed.length > 0 ) ? missed : undefined;
 }
 
