26d518b4d2db69ed935006abe9dfc0b6a33996a0
acarazo <alberto.carazo@gmail.com>
2023-08-02 15:33:30 +0200

    Suma todos los números impares de Fibonacci

---
+++ sumaFibonacci.js
@@ -0,0 +1,24 @@
+/*
+Suma todos los números impares de Fibonacci
+
+Dado un entero positivo num, devuelve la suma de todos los números impares de Fibonacci que son menores o iguales a num.
+
+Los dos primeros números de la sucesión de Fibonacci son el 0 y el 1. Cada número adicional en la secuencia es la suma de los dos números anteriores. Los siete primeros números de la sucesión de Fibonacci son 0, 1, 1, 2, 3, 5 y 8.
+
+Por ejemplo, sumFibs(10) debe devolver 10 porque todos los números impares de Fibonacci menores o iguales a 10 son 1, 1, 3 y 5.
+*/
+
+function sumFibs(num) {
+    return num;
+}
+
+sumFibs(4);
+
+/* 
+sumFibs(1) debe devolver un número.
+sumFibs(1000) debe devolver 1785.
+sumFibs(4000000) debe devolver 4613732.
+sumFibs(4) debe devolver 5.
+sumFibs(75024) debe devolver 60696.
+sumFibs(75025) debe devolver 135721.
+*/
