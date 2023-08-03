fbda145ab52b3cdf31d07403a943741f391597ea
acarazo <alberto.carazo@gmail.com>
2023-08-03 11:46:54 +0200

    Suma todos los números impares de Fibonacci

--- sumaFibonacci.js
+++ sumaFibonacci.js
@@ -1,10 +1,7 @@
 /*
 Suma todos los números impares de Fibonacci
-
 Dado un entero positivo num, devuelve la suma de todos los números impares de Fibonacci que son menores o iguales a num.
-
 Los dos primeros números de la sucesión de Fibonacci son el 0 y el 1. Cada número adicional en la secuencia es la suma de los dos números anteriores. Los siete primeros números de la sucesión de Fibonacci son 0, 1, 1, 2, 3, 5 y 8.
-
 Por ejemplo, sumFibs(10) debe devolver 10 porque todos los números impares de Fibonacci menores o iguales a 10 son 1, 1, 3 y 5.
 */
 
@@ -15,7 +12,7 @@ function sumFibs(num) {
     
     for (i; i <= num; i++) {
         // Los dos primeros números de la serie los coloco directamente en el array
-        if (i === 0 || i === 1 ) fibNums.push(i);
+        if (i === 0 || i === 1) fibNums.push(i);
         else {
             // Hago slice de los dos ultimos items del array, con reduce los sumo
             fibNums.push(fibNums.slice(-2).reduce((acc, item) => acc + item))
@@ -27,12 +24,35 @@ function sumFibs(num) {
             }
         }
     }
+
     // Finalmente filtro los items que son impares y los vuelvo a sumar con reduce
-    fibNums = fibNums.filter(item => item % 2 !== 0).reduce((acc,item) => acc + item)
+    fibNums = fibNums.filter(item => item % 2 !== 0).reduce((acc, item) => acc + item)
+    
+    /* Sin reduce
+    for (i; i <= num; i++) {
+        // Los dos primeros números de la serie los coloco directamente en el array
+        if (i === 0 || i === 1) fibNums.push(i);
+
+        // Sumo el penultimo y ultimo item del array y lo agrego al array
+        else {
+            fibNums.push(fibNums[i - 2] + fibNums[i - 1])
+        }
+    } // en for
+    
+    let sum = 0;
+    // Recorro el array de fibonacci con la condición de que cata item sea menor que num e impar, si lo cumple lo agrego a una suma acumulativa
+    for (let i = 0; i < fibNums.length; i++) {
+        if (fibNums[i] <= num && fibNums[i] % 2 != 0) {
+            sum += fibNums[i]
+        }
+    }
+    return sum;
+    */
+    
     return fibNums;
 }
 
-console.log(sumFibs(14));
+console.log(sumFibs(75025));
 
 /* 
 sumFibs(1) debe devolver un número.
